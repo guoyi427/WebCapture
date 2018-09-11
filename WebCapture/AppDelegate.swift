704bcefd26302593c4d536190e4b3f95b2e1290b
Gray <13311300131@189.cn>
2018-09-11 15:38:41 +0800

    Initial Commit

---
+++ WebCapture/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebCapture
+//
+//  Created by Gray on 2018/9/11.
+//  Copyright © 2018年 Gray. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WebCapture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebCapture
+//
+//  Created by Gray on 2018/9/11.
+//  Copyright © 2018年 Gray. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
